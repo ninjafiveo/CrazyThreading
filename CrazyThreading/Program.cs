efce8f8452c78e3307ad8b9008590b3c66af7fea
Michael Sekol <mjsekol@live.com>
2020-09-15 10:21:02 -0400

    Add project files.

---
+++ CrazyThreading/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunNoThreading();
+        }
+
+        static void RunNoThreading()
+        {
+            ThreadingSamples.NoThreading.NoThreadHere();
+        }
+    }
+}
---
+++ CrazyThreading/ThreadingSamples/NoThreading.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyThreading.ThreadingSamples
+{
+    class NoThreading
+    {
+        public static void NoThreadHere()
+        {
+            NoThread1();
+            NoThread2();
+        }
+
+        static void NoThread1()
+        {
+            while (true)
+            {
+                Console.WriteLine("This is THREAD 1");
+            }
+        }
+
+        static void NoThread2()
+        {
+            while (true)
+            {
+                Console.WriteLine("This is 22222222");
+            }
+  
+        }
+
+
+    }
+}
